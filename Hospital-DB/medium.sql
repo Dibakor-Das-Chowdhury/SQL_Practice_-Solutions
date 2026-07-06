f85221c265c079b232b7838b85beb2eb3f109165
Dibakor Das Chowdhury <48495483+Dibakor-Das-Chowdhury@users.noreply.github.com>
2026-07-05 21:19:59 -0400

    10 medium level question solved on 5th July 2026
    Added SQL queries for patient admissions, city patient counts, and roles of patients and doctors.

--- Hospital-DB/medium.sql
+++ Hospital-DB/medium.sql
@@ -28,9 +28,20 @@ select (select count(*) from patients where gender='M') as male_count, count (*)
 --Q7.Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
 select first_name,last_name,allergies from patients where allergies in('Penicillin','Morphine') order by allergies,first_name,last_name;
 
---Q8.
---Q9.
---Q10.
+--Q8.Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
+--Tricky, we have to know how the groupby clause for the two columns work?
+select patient_id, diagnosis from admissions group by patient_id, diagnosis having count (*) >1;
+
+--Q9.Show the city and the total number of patients in the city. Order from most to least patients and then by city name ascending
+select city, count(patient_id) as num_patients 
+from patients group by city order by count(patient_id) desc, city asc; 
+
+--Q10.Show first name, last name and role of every person that is either patient or doctor. The roles are either "Patient" or "Doctor"
+select first_name, last_name, 'Patient' as role from patients
+union all 
+select first_name, last_name, 'Doctor' as role from doctors;
+
 --Q11.
+
 --Q12.
 
