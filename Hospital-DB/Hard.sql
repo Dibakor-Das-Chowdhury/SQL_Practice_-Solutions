d945200ecbb68edb10acabdb33da850004ff9cab
Dibakor Das Chowdhury <48495483+Dibakor-Das-Chowdhury@users.noreply.github.com>
2026-08-14 00:19:30 -0400

    Implement admission cost calculation by insurance status
    Add SQL query to calculate admission costs based on insurance status.

--- Hospital-DB/Hard.sql
+++ Hospital-DB/Hard.sql
@@ -27,6 +27,24 @@ select distinct
   )
 from patients p
   join admissions ad on p.patient_id = ad.patient_id;
---Q3.
+
+--Q3.Each admission costs $50 for patients without insurance, and $10 for patients with insurance. All patients with an even patient_id have insurance.
+--Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group.
+
+SELECT
+  case
+    when patient_id % 2 = 0 then 'Yes'
+    else 'No'
+  end as has_insurance,
+  
+  sum (
+    case
+      when patient_id % 2 = 0 then 10
+      else 50
+    end
+  )
+ 
+FROM admissions
+group by has_insurance;
 
 
