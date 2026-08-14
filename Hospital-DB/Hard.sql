c2d581ead4fc5d001b97cfbf6bb6cdfed662517b
Dibakor Das Chowdhury <48495483+Dibakor-Das-Chowdhury@users.noreply.github.com>
2026-08-14 00:40:24 -0400

    Add query to sort provinces with Ontario first
    Add SQL query to sort province names with 'Ontario' on top

--- Hospital-DB/Hard.sql
+++ Hospital-DB/Hard.sql
@@ -48,3 +48,18 @@ FROM admissions
 group by has_insurance;
 
 
+--Peculiar Question
+--Q4. Sort the province names in ascending order in such a way that the province 'Ontario' is always on top
+SELECT province_name
+FROM province_names
+order by 
+
+(case when province_name='Ontario' then 0
+
+else 1
+
+end),
+
+province_name
+;
+;
